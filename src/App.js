2828254dd418900f36cde6f172f52fb99ec7b661
sanddot <sanginta@gmail.com>
2024-11-16 15:29:09 -0800

    Added footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ export default function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <footer>
+        <p>
+          This project was coded by Sandra Domeikiene and is{" "}
+          <a
+            href="https://github.com/sanddot/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub.
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
