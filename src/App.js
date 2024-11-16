a2b62b0dc6aae8d5e5f7ec5dc77a98498e346273
sanddot <sanginta@gmail.com>
2024-11-16 15:38:59 -0800

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <footer>
-        <p>
-          This project was coded by Sandra Domeikiene and is{" "}
-          <a
-            href="https://github.com/sanddot/react-weather-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on GitHub.
-          </a>
-        </p>
-      </footer>
+      <div className="container">
+        <h1>Hello</h1>
+        <footer>
+          <p>
+            This project was coded by Sandra Domeikiene and is{" "}
+            <a
+              href="https://github.com/sanddot/react-weather-app"
+              target="_blank"
+              rel="noreferrer"
+            >
+              open-sourced on GitHub.
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
